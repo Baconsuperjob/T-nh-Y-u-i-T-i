af74a2524d9c95f7ed2d23bc0ff9e525beda8382
Baconsuperjob <114800582+Baconsuperjob@users.noreply.github.com>
2022-10-01 14:44:04 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -1,10 +1,11 @@
 const CONFIGDATA = {
-  titleHeader: "Làm người yêu tớ nhé I Love You",
+  titleHeader: "",
   descriptionHeader:
-    "Nếu cậu thoát ra và không trả lời thì có nghĩa là cậu đã ĐỒNG Ý rồi đấy ^^",
-  buttonYes: "Đồng Ý",
-  buttonNo: "Không! Còn lâu nhé.",
-  titleModar: "Cảm ơn em vì đồng ý",
+    "Này Hà Vy tôi có điều muốn nói với cậu ^^",
+  buttonYes: "Nói đi",
+  buttonNo: "Thôi đi nha!!.",
+  titleModar: "Tôi nói đây",
   descriptionModar:
-    'Tớ biết là cậu sẽ đồng ý làm người yêu tớ mà ^^ </br> Cám ơn cậu đã đồng ý để cho tớ được che chở và bảo vệ cậu.Đối với tớ, cậu mãi là cô bé :)) </br> "Thích cậu nhiều đến mấy, cậu sẽ hiểu thôi. </br>  Tớ thích cậu nhiều đến mấy, cậu sẽ hiểu thôi "',
+    'Hà Vy nói thật lòng cho tôi biết nhá. Vy có thích hoặc là yêu tôi hay không? Chứ tôi đã rất là yêu Vy từ năm chúng ta học lớp 6 rồi. Tôi không ép Vy trả lời đâu,nhưng nếu Vy trả lời là có thì chúng ta tiến thêm một bước nữa cùng nhau đi hết quãng đường đời,còn không thì cx không sao hết,chúng ta vẫn làm bạn,tôi sẽ chúc Vy sớm tìm được hạnh phúc riêng của mình, một bến đỗ mới,tôi hơi buồn một tí thôi.Không biết tôi còn cơ hội được ngỏ lời cùng Vy không? Nhưng những điều tôi nói đều là thật lòng, còn giờ tôi sẽ chờ Vy!!! 
+Mong Vy trả lời! :love_letter:  "',
 };
