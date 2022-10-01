8d8468cddecb0a4b9b01c7a76326d44937cbc8eb
Baconsuperjob <114800582+Baconsuperjob@users.noreply.github.com>
2022-10-01 15:48:20 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -4,7 +4,7 @@ const CONFIGDATA = {
     "Này Hà Vy tôi có điều muốn nói với cậu ^^",
   buttonYes: "Nói đi",
   buttonNo: "Thôi đi nha!!.",
-  titleModar: "Yêu Vy nhiều lắm!!!",
+  titleModar: "Tôi nói đây!!!",
   descriptionModar:
     'Hà Vy nói thật lòng cho tôi biết nhá. Vy có thích hoặc là yêu tôi hay không? Chứ tôi đã rất là yêu Vy từ năm chúng ta học lớp 6 rồi. Tôi không ép Vy trả lời đâu,nhưng nếu Vy trả lời là có thì chúng ta tiến thêm một bước nữa cùng nhau đi hết quãng đường đời,còn không thì cx không sao hết,chúng ta vẫn làm bạn,tôi sẽ chúc Vy sớm tìm được hạnh phúc riêng của mình, một bến đỗ mới,tôi hơi buồn một tí thôi.Không biết tôi còn cơ hội được ngỏ lời cùng Vy không? Nhưng những điều tôi nói đều là thật lòng, còn giờ tôi sẽ chờ Vy!!! 
 Mong Vy trả lời! :love_letter:  "',
